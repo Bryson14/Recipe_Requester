eaf0937177bcf1d8b0f0b3aabb98bc01407eb202
Bryson <bryson.meiling@gmail.com>
2021-07-21 20:13:19 -0400

    outline of first page done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from "react-dom"
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Welcome from "./components/welcome";
 
-function App() {
+export default function App() {
   return (
     <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <p>I demand to to speak with your manager! - Karen</p>
-        </header>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/">
+            <Welcome requester="Bryson"/>
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
-      
     </Router>
-    
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
---
+++ src/components/welcome.js
@@ -0,0 +1,26 @@
+import {Link} from 'react-router-dom';
+
+function Welcome ({requester}) {
+
+    const imgCSS = {
+        'width': '200px',
+        'height': '300px',
+        'object-fit': 'cover'
+    }
+    
+    return (
+        <div className="vert-split-div">
+            <h3 className="span-left">Recipe Grabber</h3>
+            <Link to="/recipe-name">
+                <button className="span-left">Start</button>
+            </Link>
+            <h1  className="span-left">{requester} wants a recipe from you!</h1>
+            <p  className="span-left">5 Minutes now saves a recipe for a generation.</p>
+            <img  className="img-vert-half span-right" src={process.env.PUBLIC_URL + '/images/roberto-valdivia-rcUw6b4iYe0-unsplash.jpg'} 
+            alt="Homemade pizza which has been passed down for generations" />
+
+        </div>
+    )
+}
+
+export default Welcome;
